f63e7d38debd93e14dd586dc5e7c43ce52d3727b
Alice NIEZGODZKI <aniezgod@e2r8p2.clusters.42paris.fr>
2022-02-17 15:53:03 +0100

    in progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aniezgod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 14:44:15 by aniezgod          #+#    #+#             */
-/*   Updated: 2022/02/17 11:21:18 by aniezgod         ###   ########.fr       */
+/*   Updated: 2022/02/17 15:50:08 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -14,14 +14,28 @@
 int	line(char *buffer, int i)
 {
 	if (!buffer)
-		return (0);
-	while (buffer[i])
+		return (-1);
+	while (1)
 	{
-		if(buffer[i] == '\n' || buffer[i] == '\0')
+		if(buffer[i] == '\n' || buffer[i] == 0)
 			return (i);
 		i++;
 	}
 	return(-1);
+}
+
+int	new_line(char *buffer)
+{
+	int i = 0;
+	if (!buffer)
+		return (0);
+	while(buffer[i])
+	{
+			if(buffer[i] == '\n')
+					return (1);
+			i++;
+	}
+	return (0);
 }
 
 char *get_next_line(int fd)
@@ -31,42 +45,45 @@ char *get_next_line(int fd)
 	static char *chaine;
 	char *line_read;
 	int j;
-	int k;
 	
 	j = 0;
-	k = 0;
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	while ((i = read(fd, buffer, BUFFER_SIZE)) && line(chaine, j) != -1)
+	i = 1;
+	while ((i > 0) && new_line(chaine) == 0)
 	{
-		if (i <= 0)
-			break;
+		i = read(fd, buffer, BUFFER_SIZE);
 		buffer[i] = '\0';
 		chaine = ft_strjoin(chaine, buffer);
-		printf("%s\n", chaine);
 	}
-	printf("j = %d\n", j);
-	j = line(buffer, j);
-	printf("j = %d\nk = %d\n", j, k);
-	line_read = ft_substr(buffer, k, (line(buffer, j) - k));
-	k = j;
-	printf("j = %d\nk = %d\n", j, k);
-	chaine = ft_substr(buffer, j, (line(buffer, j) - k));
-	printf("j = %d\nk = %d\n", j, k);
+	line_read = ft_substr(chaine, 0, (line(chaine, j) + 1));
+	//printf("line = %s\n", line_read);
+	j = line(chaine, j) + 1;
+	//printf("j = %d\n", j);
+	chaine = ft_substr(chaine, j, (ft_strlen(chaine) - j));
+	//printf("save = %s\n\n\n\n\n\n", chaine);
 	return(line_read);
 }
+
+/*
 #include <stdio.h>
 int main(int argc, char **argv)
 {
 	int fd;
 	char *line;
+	int i = 20;
 
 	if(argc < 2)
-		return (1);
-	fd = open(argv[1], O_RDONLY);
+		return(1);
+	fd = open (argv[1], O_RDONLY);
 	line = get_next_line(fd);
-	printf("%s\n", line);
+	while(i--)
+	{
+		printf("%s", line);
+		line = get_next_line(fd);
+	}
 	close(fd);
 	return (0);
 }
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aniezgod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 19:50:02 by aniezgod          #+#    #+#             */
-/*   Updated: 2022/02/03 15:06:44 by aniezgod         ###   ########.fr       */
+/*   Updated: 2022/02/17 14:39:27 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -17,8 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
-//	if (!s1 || !s2)
-//		return (NULL);
+	if (!s1 && !s2)
+		return (NULL);
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
 	chaine = (char *)malloc(sizeof(*s1) * (i + j + 1));
@@ -78,26 +78,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		j++;
 		i++;
 	}
+	if (j == 0)
+		return (NULL);
 	chaine[j] = '\0';
 	return (chaine);
 }
-
-/*
- Je suis un test
- mai
- 
- is
- balek
- frero
-
-	TANTQUE(i > 1 && pas de \n dans la static)
-	{
-		i = read
-		buffer[i] = 0
-		static = strjoin(static, buffer)
-	}
-	line = fonction qui return jusqua \n;
-	static = fonction qui return tout apres \n;
-	return (line);
-
-*/
